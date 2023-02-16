40c14cdf9c0734f49ca72dbfc5282735d15bb484
Roshan Patil <patilroshan450@gmail.com>
2023-02-16 16:26:49 +0530

    google auth for mobile

--- frontend/src/components/Skills.js
+++ frontend/src/components/Skills.js
@@ -47,30 +47,32 @@ const Skills = () => {
   ];
   return (
     <>
-      <h1 className="title">Skills</h1>
       <div className="skills">
-        {skills?.map((skill) => {
-          return (
-            <div className="skill-card">
-              <motion.img
-                variants={variants}
-                initial="hidden"
-                whileInView="visible"
-                className="skill-img"
-                src={skill.image}
-                alt=""
-              />
-              <motion.h1
-                variants={variants}
-                initial="hidden"
-                whileInView="visible"
-                className="skill-title"
-              >
-                {skill.title}
-              </motion.h1>
-            </div>
-          );
-        })}
+        <div className="skill-card">
+          <h1 className="heading">Passionate About</h1>
+          <div className="contents">
+            <h1>CSS</h1>
+          </div>
+        </div>
+        <div className="skill-card">
+          <h1 className="heading">Expertise In</h1>
+          <div className="contents">
+            <h1>HTML</h1>
+            <h1>CSS</h1>
+            <h1>JavaScript</h1>
+            <h1>React</h1>
+            <h1>Bootstrap</h1>
+            <h1>Framer Motion</h1>
+          </div>
+        </div>
+        <div className="skill-card">
+          <h1 className="heading">Knows About</h1>
+          <div className="contents">
+            <h1>NodeJs</h1>
+            <h1>Express</h1>
+            <h1>MongoDB</h1>
+          </div>
+        </div>
       </div>
     </>
   );
