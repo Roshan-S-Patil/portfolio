e1257e5d87681e4cae477b2ca8f7b3cf4ef75be5
Roshan Patil <patilroshan450@gmail.com>
2023-02-16 17:11:42 +0530

    resume word docx

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import dp from "./port-dp.png";
 import { HashLink as Link } from "react-router-hash-link";
 import { GrDocumentDownload } from "react-icons/gr";
-import resume from "./Roshan Patil Resume 1.pdf";
+import resume from "./ROSHAN S.docx";
 
 const Hero = () => {
   const variants = {
